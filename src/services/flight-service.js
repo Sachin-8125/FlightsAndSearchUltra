03c5caeb404e5598238a9a05a664608a114df7ce
Sachin-8125 <sachin@ASPIREA715-76G>
2024-08-12 13:00:39 +0530

    Added flight search crud with filters

---
+++ src/services/flight-service.js
@@ -0,0 +1,36 @@
+const {FlightRepository,AirplaneRepository} = require('../repository/flight-repository');
+const {compareTime} = require('../utils/helper');
+
+class FlightService {
+
+    constructor (){
+        this.airplaneRepository = new AirplaneRepository();
+        this.flightrepository = new this.flightRepository();
+    }
+    async createFlight(data){
+        try {
+            if(!compareTime(data.arrivalTime,data.departureTime)){
+                throw{error:'Arrival time can not be less than departure time'};
+
+            }
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight = await this.flightrepository.createFlight({...data,totalSeats:airplane.capacity});
+            return flight;
+        } catch (error) {
+            console.log("something went wrong at service layer");
+            throw{error};
+        }
+    }
+
+    async getAllFlightData(data){
+        try {
+            const flights = await this.flightrepository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw {error};
+        }
+        
+    }
+}
+module.exports = FlightService;
